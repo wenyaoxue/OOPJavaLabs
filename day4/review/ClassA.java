6719802652c9fc9d2274b9c4ad158ade660d9671
wenyaoxue <119467979+wenyaoxue@users.noreply.github.com>
2023-06-30 01:41:00 +0000

    Update ClassA.java

--- day4/review/ClassA.java
+++ day4/review/ClassA.java
@@ -6,3 +6,9 @@ public class ClassA {
 	public static void methodThree(int i) {}
 	public static void methodFour(int i) {}
 }
+class ClassB extends ClassA {
+	public void methodOne(int i) {}
+	public void methodTwo(int i) {}
+	public static void methodThree(int i) {}
+	public static void methodFour(int i) {}
+}
